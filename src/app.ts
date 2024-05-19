1c04b1bb8d36f530efab938e2ff7736ee9610205
sadek1999 <sadekali1530@gmail.com>
2024-05-19 20:06:25 +0600

    complate project setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+// const port = 3000
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
-})
+// parser
 
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
-const mongoose = require('mongoose');
+import app from "./app";
+import config from "./app/config";
+
+import mongoose from "mongoose";
 
 
+async function main() {
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+    try{
+        await mongoose.connect(config.database_url as string);
+  
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+          })
+    }
+    catch(err){
+        console.log(err)
+
+    }
+   
+   
+  }
+
+main()
